6b6727c9c9c9063c90f876973db9a911f98aa3fc
Serhii Demchenko <svdemchenko@gmail.com>
2023-06-26 22:34:49 +0300

    first init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../src/styles/global.styles.css';
+import App from './App';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { supabase } from './services/supabase';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+);
