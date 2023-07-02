71bf00c11f20511f59eb48caba47fa9d24580916
Serhii Demchenko <svdemchenko@gmail.com>
2023-07-02 22:47:42 +0300

    sidebar

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ContentWrapper } from './SideBar.styled';
+import Form from '../Form/Form';
+
+const SideBar = () => {
+  return (
+    <ContentWrapper>
+      <Form />
+    </ContentWrapper>
+  );
+};
+
+export default SideBar;
--- src/components/SideBar/SideBar.styled.jsx
+++ src/components/SideBar/SideBar.styled.jsx
@@ -1,1 +1,12 @@
 import styled from '@emotion/styled';
+
+export const ContentWrapper = styled.div`
+  height: 100vh;
+  width: 150px;
+
+  display: flex;
+
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem 0rem;
+`;
