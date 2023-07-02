69c6a271e0950d0720c0254357419e78fbd7cea5
Serhii Demchenko <svdemchenko@gmail.com>
2023-07-02 22:29:54 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from './components/Layout/Layout';
@@ -7,21 +8,25 @@ import Auth from './components/Auth/Auth';
 import Calendar from './components/Calendar/Calendar';
 import PublicRoute from './components/PublicRoute/PublicRoute';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import { Loader } from './components/Loader/Loader';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route element={<PublicRoute />}>
-            <Route path="login" element={<Auth />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route element={<PublicRoute />}>
+              <Route path="login" element={<Auth />} />
+            </Route>
+            <Route element={<PrivateRoute />}>
+              <Route path="calendar" element={<Calendar />} />
+            </Route>
           </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path="calendar" element={<Calendar />} />
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
+
       <ToastContainer autoClose={2000} theme="light" />
     </>
   );
