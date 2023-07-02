00833eb01e0604c177a99c4af686ceedf4537a4c
Serhii Demchenko <svdemchenko@gmail.com>
2023-07-02 20:11:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/gcalendar">
     <React.StrictMode>
       <SessionContextProvider supabaseClient={supabase}>
         <ThemeProvider theme={theme}>
