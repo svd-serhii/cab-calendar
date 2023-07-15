c5d55d16cf79caaa44bb41d3e29fe675c02d28c8
Serhii Demchenko <svdemchenko@gmail.com>
2023-07-15 21:57:19 +0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,20 +1,20 @@
 import { createClient } from '@supabase/supabase-js';
-const { google } = require('googleapis');
+// const { google } = require('googleapis');
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const credentials = {
-  client_secret: process.env.REACT_APP_G_CLIENT_SECRET,
-  client_id: process.env.REACT_APP_G_CLIENT_ID,
-  redirect_uris: [process.env.REACT_APP_G_REDIRECT_URIS],
-};
+// const credentials = {
+//   client_secret: process.env.REACT_APP_G_CLIENT_SECRET,
+//   client_id: process.env.REACT_APP_G_CLIENT_ID,
+//   redirect_uris: [process.env.REACT_APP_G_REDIRECT_URIS],
+// };
 
-export const auth = new google.auth.GoogleAuth({
-  credentials,
-  scopes: ['https://www.googleapis.com/auth/calendar'],
-});
+// export const auth = new google.auth.GoogleAuth({
+//   credentials,
+//   scopes: ['https://www.googleapis.com/auth/calendar'],
+// });
 
 export default supabase;
